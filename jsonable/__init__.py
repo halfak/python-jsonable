bdd81a2baeb4c8c186421913831b89be660348ce
halfak <aaron.halfaker@gmail.com>
2015-10-01 09:40:17 -0500

    Increments version to 0.3.0.

--- jsonable/__init__.py
+++ jsonable/__init__.py
@@ -1,4 +1,4 @@
 from .type import Type, Base, JSONable, AbstractJSONable
 from .functions import to_json
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements(fname):
 
 setup(
     name="jsonable",
-    version="0.2.1",
+    version="0.3.0",
     author="Aaron Halfaker",
     author_email="ahalfaker@wikimedia.org",
     description=("An abstract class that supports json" +\
